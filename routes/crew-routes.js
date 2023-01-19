63c066f422a3a6434364ddfa12dbfa0317a6a3a3
LewisGraves <lewisg735@gmail.com>
2023-01-19 00:10:46 -0500

    server side

--- routes/crew-routes.js
+++ routes/crew-routes.js
@@ -5,27 +5,21 @@ const Crew = require('../models/crew')
 const router = express.Router()
 
 //INDEX crew
-router.get('/crew', (req, res, next) => {
+
+router.get('/crews', (req, res, next) => {
     Crew.find()
-        .then(crew => {
-            res.status(200).json({ crew: crew })
+        .then(crews => {
+            return crews.map(crew => crew)
+        })
+        .then(crews => {
+            res.status(200).json({ crews: crews })
         })
         .catch(next)
 })
 
-// router.get('/crew', (req, res, next) => {
-//     Character.find()
-//         .then(characters => {
-//             return characters.map(character => character)
-//         })
-//         .then(characters => {
-//             res.status(200).json({ characters: characters })
-//         })
-//         .catch(next)
-// })
-
 // SHOW crew/:id
-router.get('/crew/:id', (req, res, next) => {
+
+router.get('/crews/:id', (req, res, next) => {
     Crew.findById(req.params.id)
         .then(crew => {
             res.status(200).json({ crew: crew })
@@ -33,16 +27,9 @@ router.get('/crew/:id', (req, res, next) => {
         .catch(next)
 })
 
-// router.get('/characters/:id', (req, res, next) => {
-//     Character.findById(req.params.id)
-//         .then(character => {
-//             res.status(200).json({ character: character })
-//         })
-//         .catch(next)
-// })
-
 // CREATE and POST /crew
-router.post('/crew', (req, res, next) => {
+
+router.post('/crews', (req, res, next) => {
     // req.body
     // crew: {}
     Crew.create(req.body.crew)
@@ -52,16 +39,5 @@ router.post('/crew', (req, res, next) => {
         })
         .catch(next)
 })
-
-// router.post('/characters', (req, res, next) => {
-//     // req.body
-//     // character: {}
-//     Character.create(req.body.character)
-//         .then(character => {
-//             // top lvl of this object is character
-//             res.status(201).json({ character: character })
-//         })
-//         .catch(next)
-// })
 
 module.exports = router
